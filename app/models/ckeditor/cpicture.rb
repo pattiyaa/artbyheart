25b5ae6839db7d081c6de7214ba82d2b235315c4
pattiyaa <pattiyaa@gmail.com>
2016-11-28 22:44:05 +0700

    remove custom picture

--- app/models/ckeditor/cpicture.rb
+++
@@ -1,7 +0,0 @@
-class Ckeditor::cPicture < Ckeditor::Asset
-  mount_uploader :data, CloudinaryImageUploader, mount_on: :data_file_name
-  #mount_uploader :picture, CloudinaryImageUploader
-  def url_content
-    url(:content)
-  end
-end
