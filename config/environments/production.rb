7371a9f773bcac0eeeba0d3e759f4da68e98d2d3
pattiyaa <pattiyaa@gmail.com>
2016-12-14 18:28:38 +0700

    social share asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,6 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
@@ -86,5 +84,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
 end
